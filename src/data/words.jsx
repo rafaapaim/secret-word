44f2df51d9b11cc4aea1089a9ef48ebee06f98b1
Rafael Paim <r_paim@outlook.com>
2024-05-21 04:47:21 -0300

    first commit

---
+++ src/data/words.jsx
@@ -0,0 +1,27 @@
+export const wordsList = {
+    Animais: [
+        "Cachorro", "Gato", "Elefante", "Leão", "Girafa", "Zebra", "Macaco", "Tigre", "Borboleta", "Tartaruga",
+        "Pássaro", "Tubarão", "Baleia", "Cavalo", "Cobra", "Coruja", "Rinoceronte", "Crocodilo", "Pinguim",
+        "Urso", "Golfinho", "Coelho", "Raposa", "Águia", "Peixe"
+    ],
+    Alimentos: [
+        "Maçã", "Banana", "Arroz", "Feijão", "Carne", "Peixe", "Cenoura", "Tomate", "Alface", "Pão",
+        "Queijo", "Ovo", "Chocolate", "Morango", "Abacaxi", "Manga", "Melancia", "Abóbora", "Uva", "Pêssego",
+        "Batata", "Brócolis", "Frango", "Salmão", "Cebola"
+    ],
+    Cores: [
+        "Vermelho", "Azul", "Verde", "Amarelo", "Preto", "Branco", "Roxo", "Laranja", "Marrom", "Cinza",
+        "Rosa", "Turquesa", "Dourado", "Prata", "Bege", "Coral", "Índigo", "Ciano", "Violeta", "Rubro",
+        "Carmin", "Ocre", "Jade", "Safira", "Ônix"
+    ],
+    Profissões: [
+        "Médico", "Professor", "Engenheiro", "Advogado", "Arquiteto", "Policial", "Bombeiro", "Jornalista",
+        "Escritor", "Músico", "Artista", "Ator", "Cantor", "Agricultor", "Chef", "Designer", "Eletricista",
+        "Encanador", "Fotógrafo", "Programador", "Cientista", "Astronauta", "Psicólogo", "Enfermeiro", "Veterinário"
+    ],
+    Países: [
+        "Brasil", "Estados Unidos", "China", "Índia", "Rússia", "Japão", "Alemanha", "Reino Unido", "França",
+        "Canadá", "Itália", "Austrália", "Espanha", "México", "Coreia do Sul", "Indonésia", "Turquia", "Argentina",
+        "África do Sul", "Egito", "Nigéria", "Suécia", "Noruega", "Suíça", "Nova Zelândia"
+    ]
+}
